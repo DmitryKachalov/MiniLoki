d1bf1bc2fb5e6ba856675f72cda3ad061f49095f
evilx1990 <evilx.emelyanov@gmail.com>
2020-06-30 10:31:15 +0300

    fixed

--- lib/mini_loki_c/creation/samples.rb
+++ lib/mini_loki_c/creation/samples.rb
@@ -5,11 +5,14 @@ module MiniLokiC
     class Samples # :nodoc:
       def initialize(staging_table, options = {})
         @staging_table = staging_table
+        @path = "hle/samples/#{@staging_table}"
         @options = options
+
+        FileUtils.mkpath(@path)
       end
 
       def insert(sample)
-        file = "hle/samples/#{@staging_table}-#{sample[:staging_row_id]}.html"
+        file = "#{@path}/#{sample[:staging_row_id]}.html"
         File.open(file, 'wb') { |f| f.write(basic_html_substitutions_body(sample)) }
         puts "file://#{File.expand_path(file)}".green
       end
