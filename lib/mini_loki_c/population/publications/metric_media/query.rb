2759e5d3cabcda27bc639504c32c504b32ec5a02
evilx1990 <evilx.emelyanov@gmail.com>
2020-09-29 20:32:20 +0300

    add org_name to method for getting mm pubs by state

--- lib/mini_loki_c/population/publications/metric_media/query.rb
+++ lib/mini_loki_c/population/publications/metric_media/query.rb
@@ -89,7 +89,8 @@ module MiniLokiC
         end
 
         def pubs_by_passed_state_query
-          %|select c.id as id,
+          %|select o.name org_name,
+                   c.id as id,
                    c.name as name,
                    cc.id as client_id,
                    cc.name as client_name
