5bf4e0c2798e6a0c5fa8746034f2d11bce19b4b4
yart <jarocki.art@gmail.com>
2020-10-16 14:28:44 +0300

    TimeFrame module added

--- lib/mini_loki_c/population.rb
+++ lib/mini_loki_c/population.rb
@@ -4,6 +4,7 @@ require_relative 'population/formatize.rb'
 require_relative 'population/publications.rb'
 require_relative 'population/sql.rb'
 require_relative 'population/population_success.rb'
+require_relative 'population/time_frame.rb'
 
 module MiniLokiC
   module Population; end
---
+++ lib/mini_loki_c/population/time_frame.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module MiniLokiC
+  module Population
+    module TimeFrame
+      class LokiCDate < Date
+        def quarter
+          case self.month
+          when 1..3; 1
+          when 4..6; 2
+          when 7..9; 3
+          else 4
+          end
+        end
+        
+        def half_year
+          (1..6).include?(self.month) ? 1 : 2
+        end
+      end
+      
+      def self.[](period, date)
+        date = LokiCDate.parse(date)
+        
+        case period
+        when :daily;      "d:#{date.yday}:#{date.year}"
+        when :weekly;     "w:#{date.cweek}:#{date.year}"
+        when :monthly;    "m:#{date.month}:#{date.year}"
+        when :quarterly;  "q:#{date.quarter}:#{date.year}"
+        when :biannually; "b:#{date.half_year}:#{date.year}"
+        else :annually;   "#{date.year}"
+        end
+      end
+    end
+  end
+end
